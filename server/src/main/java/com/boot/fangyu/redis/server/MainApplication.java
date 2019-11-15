9d882f2d856cd05ef5fa2201471a01ff513bdccc
fangyu <34384878@qq.com>
2019-11-15 15:26:22 +0800

    配置文件

--- server/src/main/java/com/boot/fangyu/redis/server/MainApplication.java
+++ server/src/main/java/com/boot/fangyu/redis/server/MainApplication.java
@@ -1,9 +1,27 @@
 package com.boot.fangyu.redis.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
 /**
  * @author fangyu
  * @version v1.0.0
  * @since 2019/11/9 12:38 上午
  */
-public class MainApplication {
+
+@SpringBootApplication
+@ImportResource(value = {"classpath:spring/spring-jdbc.xml"})
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
